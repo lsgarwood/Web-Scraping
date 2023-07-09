ca1b23d75cc3c712dabc27afe122b868c525799c
Lauren Garwood <ls_garwood@yahoo.co.uk>
2023-07-09 14:59:27 +0100

    to get sys to work

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
 
-if __name__ == "__AccessingCLArgs/main__":
+if __name__ == "__main__":
     print(f"Arguments count: {len(sys.argv)}")
     for i, arg in enumerate(sys.argv):
         print(f"Argument {i:>6}: {arg}")
 
-        # run python AccessingCLArgs/main.py Python Command Line Arguments
+# run python AccessingCLArgs/main.py Python Command Line Arguments
